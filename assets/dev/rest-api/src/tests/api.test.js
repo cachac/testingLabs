c9dad1b3c18978eb71b7b04bc76281c8306c96e0
Car <carlos@storylabs.dev>
2024-08-19 13:40:54 -0600

    unit testing

---
+++ assets/dev/rest-api/src/tests/api.test.js
@@ -0,0 +1,25 @@
+import supertest from 'supertest'
+import { app, server } from '../main.js'
+import config from '../config/index.js'
+
+const api = supertest(app)
+
+test('healthcheck', async () => {
+  const res = await api.get('/healthcheck').send()
+  expect(res.statusCode).toEqual(200)
+})
+
+test('delay', async () => {
+  const res = await api.get('/delay?seconds=3').send()
+  expect(res.statusCode).toEqual(200)
+})
+
+// TODO: descomentar para probar.
+// test('error400', async () => {
+//   const res = await api.get('/error400').send()
+//   expect(res.statusCode).toEqual(200)
+// })
+
+afterAll(() => {
+  server.close()
+})
