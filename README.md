b8d35b5408d63234b6a7476704917dd9009a152c
Car <carlos@storylabs.dev>
2024-08-27 14:47:20 -0600

    06 + 07

--- README.md
+++ README.md
@@ -5,19 +5,26 @@
 # 3. [Construcción](./03.%20Construccion.md)
 # 4. [Automatización de pruebas unitarias](./04.%20Automatizacion%20de%20Pruebas%20Unitarias.md)
 # 5. [Automatización de análisis CVE's](./05.Automatización%20de%20análisis%20CVE.md)
+# 6. [SAST vs DAST](./06.%20Deployment%20DAST.md)
+# 7. [Load Testing](./07.%20Load%20Testing.md)
 
-# 6. Build
-sonarqube
-snyk
+# . Deploy and run
 
-
-
-# 7. Deploy and run
-1. loadTesting?
 2. selenium
 3. secrets
+deploy kubernetes (istio / api gateway / flagger ...)
 4. net pol
 
 5. monitoring
-6. loggin
-falco
+6. logging
+7. falco: https://falco.org/blog/falco-driverkit-debian-docker/
+8. Final
+
+
+Revisión de configuraciones​
+
+Monitoreo (real time)​
+
+Logging (real time)​
+
+Alertas al SOC
--- assets/dev/rest-api/src/main.js
+++ assets/dev/rest-api/src/main.js
@@ -32,24 +32,29 @@ process.on('unhandledRejection', err => {
 // health checks
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  logger.info(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3000`)
+  res.send({ app: config.APP_NAME, env: config.NODE_ENV, port: config.NODE_PORT, version: config.APP_VERSION, sticky })
+})
+
 router.get('/healthcheck', (req, res) => {
-  logger.info(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3080`)
+  logger.info(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3000`)
   res.send({ app: config.APP_NAME, env: config.NODE_ENV, port: config.NODE_PORT, version: config.APP_VERSION, sticky })
 })
 
 router.get('/error500', (req, res) => {
-  logger.error(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3080 => Error 500`)
+  logger.error(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3000 => Error 500`)
   res.status(500).send({ app: config.APP_NAME, env: config.NODE_ENV, port: config.NODE_PORT, version: config.APP_VERSION, sticky, error: 500 })
 })
 
 router.get('/error400', (req, res) => {
-  logger.error(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3080 => Error 400`)
+  logger.error(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3000 => Error 400`)
   res.status(400).send({ app: config.APP_NAME, env: config.NODE_ENV, port: config.NODE_PORT, version: config.APP_VERSION, sticky, error: 400 })
 })
 
 router.get('/delay', async (req, res) => {
   const { seconds } = req.query
-  logger.info(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3080 => delay ${seconds} seconds`)
+  logger.info(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3000 => delay ${seconds} seconds`)
 
   await new Promise(resolve => setTimeout(resolve, seconds * 1000))
 
@@ -57,7 +62,7 @@ router.get('/delay', async (req, res) => {
 })
 
 router.get('/crash', (req, res) => {
-  logger.error(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3080 => CRAAAASHHHH`)
+  logger.error(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3000 => CRAAAASHHHH`)
   process.exit(1)
 })
 
@@ -68,11 +73,11 @@ router.get('/random_crash', (req, res) => {
 
   if (num === 0) {
     logger.error('💥')
-    logger.error(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3080 => RANDOM_CRAAAASHHHH #${num}`)
+    logger.error(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3000 => RANDOM_CRAAAASHHHH #${num}`)
     process.exit(1)
   }
 
-  logger.info(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3080 => RANDOM_CRAAAASHHHH #${num}`)
+  logger.info(`[${config.NODE_ENV}] App: ${config.APP_NAME} v${config.APP_VERSION}. Session: ${sticky} on Port 3000 => RANDOM_CRAAAASHHHH #${num}`)
 
   res.send({ app: config.APP_NAME, env: config.NODE_ENV, port: config.NODE_PORT, version: config.APP_VERSION, sticky, random_crash: num })
 })
